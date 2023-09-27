8dbd64f40823ca973923956cde43f808a1c1f7c9
TRBIV <trbiv@proton.me>
2023-09-26 19:36:45 -0500

    keep content in view when typing on ios, close keyboard by tapping off text input

--- app/(pages)/signing.tsx
+++ app/(pages)/signing.tsx
@@ -1,25 +1,31 @@
-import { View, Text, TextInput } from "react-native"
+import { View, KeyboardAvoidingView, Text, TextInput, Platform, Keyboard, TouchableWithoutFeedback } from "react-native"
 import NavButton from "../../components/NavButton"
 
 export default function SigningPage() {
     return (
-        <View className="flex-1 justify-between m-10">
-            <View className="flex justify-center items-center">
-                <Text>Signing Page</Text>
-            </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <KeyboardAvoidingView 
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                className="flex-1 justify-between m-10"
+            >
+                <View className="flex justify-center items-center">
+                    <Text>Signing Page</Text>
+                </View>
 
-            <View className="flex items-center justify-center">
-                <Text className="m-2">Enter Message</Text>
-                <TextInput 
-                    className="border w-4/5 h-32"
-                    editable
-                    multiline
-                    numberOfLines={6}
-                    textAlignVertical="top"
-                    scrollEnabled
-                />
-                <NavButton title="Sign" href="/signature" />
-            </View>
-        </View>
+                <View className="flex items-center justify-center">
+                    <Text className="m-2">Enter Message</Text>
+
+                    <TextInput 
+                        className="border w-64 h-32"
+                        editable
+                        multiline
+                        numberOfLines={5}
+                        textAlignVertical="top"
+                        scrollEnabled
+                    />
+                    <NavButton title="Sign" href="/signature" />
+                </View>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     )
 }
--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -8,7 +8,7 @@ interface NavButtonProps {
 
 export default function NavButton(props: NavButtonProps) {
     return (
-        <View className='w-4/5 m-2'>
+        <View className='w-64 m-2'>
             <Link href={props.href} asChild>
                 <Button title={props.title} />
             </Link>
